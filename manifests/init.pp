054998c6ef79be07c43bcb2473846a6818d3a91d
Idris Mokhtarzada <idris.m@gmail.com>
2013-02-24 00:18:40 -0500

    Fix CATALINA_HOME export

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,9 +14,7 @@ class tomcat6 {
     ensure => '6.0.36-boxen1'
   }
 
-  $exports = "export BOXEN_CATALINA_HOME=${homebrew::config::installdir}/Cellar/tomcat6/6.0.36-boxen1/libexec
-    export CATALINA_HOME=$BOXEN_CATALINA_HOME
-    "
+  $exports = "export BOXEN_CATALINA_HOME=${homebrew::config::installdir}/Cellar/tomcat6/6.0.36-boxen1/libexec\nexport CATALINA_HOME=\$BOXEN_CATALINA_HOME"
 
   file { "${boxen::config::envdir}/tomcat6.sh":
     content => "$exports",
